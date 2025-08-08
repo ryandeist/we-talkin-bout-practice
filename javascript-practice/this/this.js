984a901626c089a4cb64c9a668fc8d8b3ac76f24
Ryan Deist <ryandeist@gmail.com>
2025-08-08 16:59:30 -0400

    this

--- javascript-practice/this/this.js
+++ javascript-practice/this/this.js
@@ -1,11 +1,55 @@
+'use strict';
+
 // console.log(this);
+// const button = document.getElementById('btn');
 
-function logThis() {
-    console.log(this);
+// button.addEventListener('click', logThis);
+
+// function logThis(x, y) {
+//     console.log(this);
+//     console.log(x, y);
+// }
+
+// const obj = {
+//     num: 32,
+//     logThis,
+// }
+
+// obj.logThis();
+
+// const obj = {
+//     num: 32,
+// };
+
+// logThis();
+
+// const boundLogThis = logThis.bind(obj);
+
+// boundLogThis(1, 2);
+
+// logThis.call(obj, 1, 2);
+
+// logThis.apply(obj, [1, 2]);
+
+// [1, 2, 3].forEach(function(num) {
+//     console.log(this);
+//     console.log(num)
+// }, { num: 32 });
+
+class Person {
+    constructor(name) {
+        // console.log(this);
+        this.name = name;
+        // console.log(this);
+    }
+
+    sayHello() {
+        console.log(`Hello, my name is ${this.name}.`);
+    }
 }
 
-const obj = {
-    num: 32,
-    logThis,
-}
-obj.logThis();
+const john = new Person('John');
+const jane = new Person('Jane');
+
+john.sayHello();
+jane.sayHello();
