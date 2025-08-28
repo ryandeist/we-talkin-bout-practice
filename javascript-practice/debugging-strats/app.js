7ae5c8b24492af5bb24528b4a2f69c6d41be64b5
Ryan Deist <ryandeist@gmail.com>
2025-08-28 18:26:35 -0400

    debugging strats.

---
+++ javascript-practice/debugging-strats/app.js
@@ -0,0 +1,19 @@
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', moveRight);
+
+// function moveRight() {
+//     const circle = document.getElementById('circle');
+//     const { left } = getComputedStyle(circle);
+//     circle.style.left = parseInt(left) + 10 + 'px';
+// };
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', moveRight);
+
+function moveRight() {
+    const circle = document.getElementById('circle');
+    const { left } = getComputedStyle(circle);
+    circle.style.left = left + 10;
+};
