72b1903c192f3bfdc0beecbb99d39f7d174e9164
Ryan Deist <ryandeist@gmail.com>
2025-09-03 21:34:01 -0400

    misc sandbox

--- javascript-practice/sandbox/app.js
+++ javascript-practice/sandbox/app.js
@@ -1,29 +1,12 @@
+unflattenedArray = [{a: 1, b: {c: 2, d: 3}}]
+
 function flatten(value) {
-    if (typeof value !== 'object' || value === null) {
-        return value;
-    }
-
-    if (typeof value === 'object') {
-        const result = {};
-
-        for (const [key, value] of Object.entries(value)) {
-            result[key] = flatten(value);
-        }
-    }
-
-    if (Array.isArray(value)) {
-        const result = [];
-
-        for (const item of value) {
-            if (Array.isArray(item)) {
-                result.push(flatten(item));
-            } else {
-                result.push(item);
-            }
-        }
-
-        return result;
-    }
-
-
+    if (typeof value !== 'object' || value === n)
+    if 
+    arr.forEach((val) => {
+        console.log(val);
+    })
 }
+
+flatten(unflattenedArray);
+
